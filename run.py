c3fbc046b7ae2d5a7ac06229f667191c75bd74b6
paddyhockey1 <patjoegalvin@gmail.com>
2024-07-24 08:27:42 +0000

    Add: code to randomize answers and return in uppercase

--- run.py
+++ run.py
@@ -2,3 +2,10 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import random
+
+from teams import team_list
+
+def get_word():
+    word = random.choice(team_list)
+    return word.upper()
