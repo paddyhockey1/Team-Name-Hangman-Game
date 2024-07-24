e40f57ee257fce1aecb77f83e725e6e37a7c59ca
paddyhockey1 <patjoegalvin@gmail.com>
2024-07-24 08:25:36 +0000

    Add: link to interanl teams page

--- run.py
+++ run.py
@@ -2,6 +2,3 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-import random
-
-from teams import team_list
