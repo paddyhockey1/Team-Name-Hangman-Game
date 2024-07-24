96c8d5ef5c14ffc3dab2ca189d5f01f06e9ca9b3
paddyhockey1 <patjoegalvin@gmail.com>
2024-07-24 07:32:08 +0000

    Add: American league teams to team page

--- teams.py
+++ teams.py
@@ -11,6 +11,8 @@ teram_list = [
     "orioles", "red sox", "yankees", "blue jays", "rays",
     "white sox", "guardians", "tigers", "twins", "royals",
     "astros", "athletics", "angels", "mariners", "rangers",
-    
+    "mets", "phillies", "nationals", "marlins", "braves",
+    "cardinals", "pirates", "brewers", "cubs", "reds",
+    "dodgers", "diamondbacks", "giants", "padres", "rockies",
 
 ]
