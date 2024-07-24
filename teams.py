741031818512f2af5500f6cb47b297e51517db6b
paddyhockey1 <patjoegalvin@gmail.com>
2024-07-24 07:25:02 +0000

    Add: NFC teasm to teams page

--- teams.py
+++ teams.py
@@ -2,5 +2,6 @@ teram_list = [
     "bears", "lions", "vikings", "packers",
     "falcons", "saints", "buccaneers", "panthers", 
     "cowboys", "eagles", "commanders", "giants",
+    "niners", "cardinals", "rams", "seahawks",
     
 ]
