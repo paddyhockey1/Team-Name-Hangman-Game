28e747b4f56ddeb8689ef1c74fddaf2fcbb6d935
paddyhockey1 <patjoegalvin@gmail.com>
2024-07-25 11:30:33 +0100

    Change: both underscores to minus signs

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ def play(word):
             for index in indices:
                 word_as_list[index] = guess
                 word_completion = "".join(word_as_list)
-                if "_" not in word_completion:
+                if "-" not in word_completion:
                     guessed = True
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
