a871ade71d18b2ae4a62ac558b373d223f17680c
paddyhockey1 <patjoegalvin@gmail.com>
2024-07-25 11:27:17 +0100

    Fix: indent and whitesapce issues

--- teams.py
+++ teams.py
@@ -1,9 +1,9 @@
 team_list = [
-   
-   #ADD NATIONAL FOOTBALL LEAGUE (NFL) TEAMS LIST HERE
-   
+
+   # ADD NATIONAL FOOTBALL LEAGUE (NFL) TEAMS LIST HERE
+
     "bears", "lions", "vikings", "packers",
-    "falcons", "saints", "buccaneers", "panthers", 
+    "falcons", "saints", "buccaneers", "panthers",
     "cowboys", "eagles", "commanders", "giants",
     "niners", "cardinals", "rams", "seahawks",
     "bills", "jets", "patriots", "dolphins",
@@ -11,7 +11,7 @@ team_list = [
     "colts", "jaguars", "titans", "texans",
     "chiefs", "broncos", "raiders", "chargers",
 
-    #ADD MAJOR LEAGUE BASEBALL (MLB) TEAMS LIST HERE
+    # ADD MAJOR LEAGUE BASEBALL (MLB) TEAMS LIST HERE
 
     "orioles", "redsox", "yankees", "blue jays", "rays",
     "whitesox", "guardians", "tigers", "twins", "royals",
@@ -20,23 +20,23 @@ team_list = [
     "cardinals", "pirates", "brewers", "cubs", "reds",
     "dodgers", "diamondbacks", "giants", "padres", "rockies",
 
-    #ADD NATIONAL HOCKEY LEAGUE (NHL) TEAM LIST HERE
+    # ADD NATIONAL HOCKEY LEAGUE (NHL) TEAM LIST HERE
 
-    "maple leafs", "canadiens", "senators", "panthers", 
+    "maple leafs", "canadiens", "senators", "panthers",
     "lightning", "bruins", "sabres", "redwings",
-    "hurricanes", "rangers", "devils", "islanders", 
+    "hurricanes", "rangers", "devils", "islanders",
     "flyers", "capitals", "penguins", "bluejackets",
-    "avalanche", "stars", "jets", "predators", 
+    "avalanche", "stars", "jets", "predators",
     "blues", "blackhawks", "wild", "coyotes",
     "kings", "ducks", "sharks", "kraken",
     "canucks", "oilers", "flames", "goldenknights",
 
-    #ADD NATIONAL BASKETBALL ASSOCIATION TEAMS HERE
+    # ADD NATIONAL BASKETBALL ASSOCIATION TEAMS HERE
 
-    "celtics", "nets", "knicks", "sixers", "raptors", 
+    "celtics", "nets", "knicks", "sixers", "raptors",
     "bulls", "cavaliers", "pistons", "bucks", "pacers",
-    "hawks", "hornets", "heat", "magic", "wizards", 
-    "nuggets", "timberwolves", "thunder", "trailblazers", "jazz", 
-    "warriors", "clippers", "lakers", "suns", "kings", 
+    "hawks", "hornets", "heat", "magic", "wizards",
+    "nuggets", "timberwolves", "thunder", "trailblazers", "jazz",
+    "warriors", "clippers", "lakers", "suns", "kings",
     "mavericks", "rockets", "spurs", "grizzlies", "pelicans",
 ]
